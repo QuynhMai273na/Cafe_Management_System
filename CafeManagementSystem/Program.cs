663c4c300bfcd8661c8b65593970487cfb26d90d
anniehayho <21522697@gm.uit.edu.vn>
2023-12-19 21:28:33 +0700

    sửa login

--- CafeManagementSystem/Program.cs
+++ CafeManagementSystem/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using CoffeeManagerApplication;
 
 namespace CafeManagementSystem
 {
@@ -16,7 +17,7 @@ namespace CafeManagementSystem
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form());
+            Application.Run(new fLogin());
         }
     }
 }
