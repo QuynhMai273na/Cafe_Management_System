ae4f55b1a36ff96509f1d7a5cf50353f03a64278
ADMIN\Admin <21522392@gm.uit.edu.vn>
2023-12-20 13:56:34 +0700

    fix chu hoa chu thuong

--- CafeManagementSystem/DAO/AccountDAO.cs
+++ CafeManagementSystem/DAO/AccountDAO.cs
@@ -24,6 +24,18 @@ namespace CafeManagementSystem.DAO
         {
             string query = "USP_Login @userName , @password";
             DataTable result = DataProvider.Instance.ExecuteQuery(query, new object[] {username , password});
+            //Them vao database (sql) de fix loi chu hoa chu thuong
+            /*
+             ALTER PROCEDURE USP_Login @userName NVARCHAR(100), @passWord NVARCHAR(100)
+                AS
+                BEGIN
+	            SELECT * FROM dbo.Account 
+	            WHERE Account.userName COLLATE SQL_Latin1_General_CP1_CS_AS = @userName 
+	             Account.passWord COLLATE SQL_Latin1_General_CP1_CS_AS = @passWord
+                END
+                GO
+            nhớ reload connection
+             */
             //kiểm tra xem biến "result" có chứa dữ liệu không.
             //Nếu số hàng (rows) trong "result" > 0, tức là có ít nhất một hàng dữ liệu, thì trả về true. Ngược lại thì trả về false.
             return result.Rows.Count > 0;
